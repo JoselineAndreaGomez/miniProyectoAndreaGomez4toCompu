1e7ab8d25d793625ee7f132598a93b141bdbc440
unknown <gomezvjoselineasj@gmail.com>
2022-09-16 09:04:01 -0600

    actualizaciones en la agenda telefonica

--- Agenda.py
+++ Agenda.py
@@ -1,20 +1,23 @@
 from agendaTele import agentaTelefonica
 registro=[]
 
-#Creacion de objetos
-def nuevoRegistro(nombreCliente, noDPI, direccion):
+#Crear contactos
+def nuevocontacto(nombreCliente, noDPI, direccion):
     registro.append(agentaTelefonica(nombreCliente,noDPI,direccion))
 
-#Mostrar los registros por nombre
 
-def mostrarRegistros():
+#mostrar contactos
+def mostrarcontactos():
     for i in range(len(registro)):
         print(registro[i].verNombre())
+        print(registro[i].verNoDPI())
+        print(registro[i].verdireccion())
+        print('\n\n')
     print('------------------------------------------')
 
-#Buscar registro y actualizar datos
+#Buscar contacto y modificar
 
-def buscarRegistroActualizar(nombreCliente, nuevoDPI, nuevaDireccion):
+def buscarymodificar(nombreCliente, nuevaDireccion):
     posicion=None
     for i in range (len(registro)):
         if nombreCliente==registro[i].verNombre():
@@ -23,12 +26,11 @@ def buscarRegistroActualizar(nombreCliente, nuevoDPI, nuevaDireccion):
     if posicion==None:
         return 'El cliente ingresado no existe'
     else:
-        registro[posicion].modificarDPI(nuevoDPI)
         registro[posicion].modificardireccion(nuevaDireccion)
         return 'Datos del cliente actualizados con éxito'
 
-#Buscar el registro para obtener la posición y eliminar el registro
-def buscarRegistroEliminar(nombreCliente):
+#Buscar y eliminar
+def buscaryeliminar(nombreCliente):
     posicion= None
     for i in range(len(registro)):
         if nombreCliente==registro[i].verNombre():
@@ -40,40 +42,39 @@ def buscarRegistroEliminar(nombreCliente):
         registro.pop(posicion)
         return 'Cliente eliminado con éxito'
 
-#arranque del programa
 
+#ARRANQUE DEL PROGRAMA------------------------------------------------------------
 def main():
     op=0
     while op!=5:
-        print('CONTROL DE LA AGENDA TELEFÓNICA \n',
-        '1. Añadir nuevo registro \n',
-        '2. Ver clientes registrados \n',
-        '3. Modificar registro de un cliente \n',
-        '4. Eliminar cliente\n',
-        '5. SALIR DEL CONTROL DE TELEFONÍA \n\n'
+        print('AGENDA TELEFÓNICA \n',
+        '1. Crear contacto \n',
+        '2. Ver contactos \n',
+        '3. Modificar contactos \n',
+        '4. Eliminar contacto\n',
+        '5. SALIR DE LA AGENDA TELEFÓNICA\n\n'
         )
         op=int(input('Ingrese una opción del menú: '))
         if op==1:
             nombre=input('Ingrese el nombre del cliente: ')
             dpi=int(input('Ingrese el número de DPI del cliente: '))
             direccion=input('Ingrese la direccion del cliente: ')
-            nuevoRegistro(nombre,dpi,direccion)
+            nuevocontacto(nombre,dpi,direccion)
             print('------------------Cliente registrado con éxito------------------\n\n')
         
         elif op==2:
-            mostrarRegistros()
-            print('\n\n')
+            mostrarcontactos()
+            print('\n')
         
         elif op==3:
             nombre=input('Ingrese el nombre del cliente: ')
-            nuevoDpi=int(input('Ingrese el nuevo DPI del cliente: '))
             nuevadireccion=input('Ingrese nueva dirección del cliente: ')
-            print(buscarRegistroActualizar(nombre,nuevoDpi,nuevadireccion))
+            print(buscarymodificar(nombre,nuevadireccion))
             print('------------------------------------------------------\n\n')
         
         elif op==4:
             nombre=input('Ingrese el nombre del cliente: ')
-            print(buscarRegistroEliminar(nombre))
+            print(buscaryeliminar(nombre))
             print('------------------------------------------------------\n\n')
         elif op==5:
             print('------------------SALIENDO DEL PROGRAMA------------------\n\n')
